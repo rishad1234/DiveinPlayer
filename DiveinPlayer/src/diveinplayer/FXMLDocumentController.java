cace8ff1b5fe12351b6423665b1609a00cd8e525
Rishadul Islam <rishadulislamkhan@gmail.com>
2018-06-11 22:28:12 +0600

    video player is active now

--- DiveinPlayer/src/diveinplayer/FXMLDocumentController.java
+++ DiveinPlayer/src/diveinplayer/FXMLDocumentController.java
@@ -1,17 +1,29 @@
 
 package diveinplayer;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.ToolBar;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class FXMLDocumentController implements Initializable {
     
@@ -23,6 +35,8 @@ public class FXMLDocumentController implements Initializable {
     private StackPane MainPane;
     @FXML
     private ToolBar toolBar;
+    @FXML
+    private MenuItem videoPlayerButton;
     
     private double posX;
     private double posY;
@@ -53,6 +67,7 @@ public class FXMLDocumentController implements Initializable {
         Platform.exit();
         System.exit(0);
     }
+    
     /*
     this method handles the minimize mutton of the mp3 player app
     */
@@ -63,5 +78,32 @@ public class FXMLDocumentController implements Initializable {
             Stage stage = (Stage) MainPane.getScene().getWindow();
             stage.setIconified(true);
         });
-    } 
+    }
+    
+    /*
+    this method handles the new video player fxml file and opens
+    the video player
+    */
+    
+    @FXML
+    public void VideoPlayerButtonAction(ActionEvent event){
+        videoPlayerButton.addEventHandler(EventType.ROOT, new EventHandler(){
+            @Override
+            public void handle(Event event) {
+                try {
+                    Parent root = FXMLLoader.load(getClass().getResource("/Video/VideoPlayerFXML.fxml"));
+                    Scene scene = new Scene(root);
+
+                    Stage stage = new Stage();
+                    stage.setScene(scene);
+                    stage.show();
+                    stage.getIcons().add(new Image(getClass().getResourceAsStream("diveIn Final.png")));
+                    stage.setTitle("Divein");
+                } catch (IOException ex) {
+                    Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+    }
+    
 }
