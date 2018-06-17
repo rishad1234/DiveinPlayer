5e7d7872dc6d15dd853801a83d914bd5b4c403f7
Rishadul Islam <rishadulislamkhan@gmail.com>
2018-06-17 23:51:33 +0600

    song data class added to find out the name and album of the song

--- AllFileSearchExample/src/allfilesearchexample/AllFileSearchExample.java
+++ AllFileSearchExample/src/allfilesearchexample/AllFileSearchExample.java
@@ -1,6 +1,7 @@
 
 package allfilesearchexample;
 
+import static allfilesearchexample.SongData.songPath;
 import allfilesearchexample.search.Drive;
 import static allfilesearchexample.search.getDrives;
 import java.io.File;
@@ -9,12 +10,10 @@ import java.util.List;
 
 
 public class AllFileSearchExample {
-
-
+    static List<String> rootPaths = new ArrayList<>();
+    static List<String> paths = new ArrayList<>();
     public static void main(String[] args) {
         search s = new search();
-        
-        List<String> rootPaths = new ArrayList<>(); 
         
         List<Drive> drives = getDrives();
         drives.forEach((drive) -> {
@@ -28,23 +27,41 @@ public class AllFileSearchExample {
         
         rootPaths.forEach((root) -> {
             File[] file = new File(root).listFiles();
-            showFiles(file);
+            showFiles(file, ".mp3");
             //System.out.println(root);
         });
+        
+        showName();
     }
     
-    public static void showFiles(File[] files) throws NullPointerException{
+    public static void showFiles(File[] files, String extension) throws NullPointerException{
         for(File file : files){
             if(file.isDirectory()){
                 //System.out.println(file.toString());
                 if(file.exists() && file.listFiles() != null)
-                    showFiles(file.listFiles());
+                    showFiles(file.listFiles(), extension);
             }else{
-                if(file.exists() && file.getName().endsWith(".mp3")){
-                    System.out.println("File: " + file.getPath());
+                if(file.exists() && file.getName().endsWith(extension)){
+                    String path = file.getAbsolutePath();
+                    path = path.replace("\\", "\\\\");
+                    paths.add(path);
+                    System.out.println("File: " + path);
                 }
             }
         }
+    }
+    
+    public static void showName(){
+        SongData songName = new SongData();
+        for(String path : paths){
+            songName.addSong(path);
+        }
+        
+//        for(String name: songPath){
+//            System.out.println(name);
+//        }
+        
+        songName.addSongTest();
     }
     
 }
