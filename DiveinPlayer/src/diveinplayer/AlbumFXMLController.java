007398dcd5be0cd2acddd23bcfe244bac288f1b5
Rishadul Islam <rishadulislamkhan@gmail.com>
2018-07-20 02:06:12 +0600

    warnings checked

--- DiveinPlayer/src/diveinplayer/AlbumFXMLController.java
+++ DiveinPlayer/src/diveinplayer/AlbumFXMLController.java
@@ -30,6 +30,8 @@ public class AlbumFXMLController implements Initializable {
 
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
