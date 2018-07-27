7e7c41983cbbe6c5232690548d2018b4107a446d
Rishadul Islam <rishadulislamkhan@gmail.com>
2018-07-27 11:35:18 +0600

    playlist fxml edited

---
+++ DiveinPlayer/src/diveinplayer/PlayListFXMLController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diveinplayer;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author Admin
+ */
+public class PlayListFXMLController implements Initializable {
+
+    @FXML
+    private TableView<?> AlbumSongTable;
+    @FXML
+    private TableColumn<?, ?> AlbumNameColumn;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void getSelectedCellDataForAlbums(MouseEvent event) {
+    }
+    
+}
